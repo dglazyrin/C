5dfe6abd0937aecf94b411a64c1d456d3692401e
Glazyrin D <dglazyrin@gmail.com>
2023-03-03 22:19:16 +0300

    Tasks 0-3 done

---
+++ C#/Z3/What_a_day.cs
@@ -0,0 +1,6 @@
+﻿string [] day = {"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
+Console.WriteLine("День-то сегодня какой?");
+string input_day_number = Console.ReadLine();
+int day_number = int.Parse(input_day_number) - 1;
+day_number = day_number;
+Console.Write("Значит сегодня - " + day[day_number]);
