d397faee43ebfa7fae6f8ac5a31f267842a286a4
Glazyrin D <dglazyrin@gmail.com>
2023-03-04 17:46:59 +0300

    All Intern's done

--- C#/Z1/Is_2_square.cs
+++ C#/Z1/Is_2_square.cs
@@ -1,10 +1,9 @@
 ﻿int A = new Random().Next(1,100);
-int B = new Random().Next(1,100);                                 // Загадаем два числа
-int B_square = B*B;                                              // Сразу найдём квадраты этих чисел
-string Square_flag = " не";                                      // Флаг условия "В не является квадратом А"
-Console.WriteLine("Есть два числа - " + A + " и " + B);          // Вывод полученных чисел
-if (B_square==A)                                                                          // Второе является квадратом первого?
+int B = new Random().Next(1,100);
+string Square_flag = " не";
+Console.WriteLine("Есть два числа - " + A + " и " + B);
+if (B/A == A)
 {
-   Square_flag = "";                                                                      // Если нет - используем приставку "НЕ"
+   Square_flag = "";
 }
 Console.WriteLine("Число " + A + Square_flag + " является квадратом числа " + B);
---
+++ C#/Z14/Delete_third.cs
@@ -0,0 +1,10 @@
+﻿int A = new Random().Next(1,1000);
+Console.Write("У нас есть число " + A + " и его третья цифра ");
+if (((A%1000-A%100)/100) > 0)
+{
+     Console.Write((A%1000-A%100)/100);
+}
+else
+{
+     Console.Write("отсутствует.");
+}
