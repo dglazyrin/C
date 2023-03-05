d335128624d9f6ab0c449d82ff5c5755bde17484
Glazyrin D <dglazyrin@gmail.com>
2023-03-05 23:27:08 +0300

    Junior 15-16 are done

---
+++ C#/Z16/Is_vacant.cs
@@ -0,0 +1,18 @@
+﻿int WhatADay(string arg)
+{
+     int result = int.Parse(arg)-1;              // Парсим введённое число
+     return result;
+}
+string [] day = {"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"}; // Массив с днями недели
+Console.WriteLine("День-то сегодня какой?");      // Спрашиваем номер дня
+string input_day_number = Console.ReadLine();                       
+int day_number = WhatADay(input_day_number);
+Console.Write("Значит сегодня - " + day[day_number]);
+if((day_number+1)<6)
+{
+     Console.WriteLine(", идите работать!");
+}
+else
+{
+     Console.WriteLine(", отдыхайте!");
+}
--- C#/Z3/What_a_day.cs
+++ C#/Z3/What_a_day.cs
@@ -2,5 +2,4 @@
 Console.WriteLine("День-то сегодня какой?");
 string input_day_number = Console.ReadLine();                                                  // Спрашиваем номер дня
 int day_number = int.Parse(input_day_number)-1;                                              // Парсим введённое число
-day_number = day_number;                                                                       // Ищем название дня в массиве
 Console.Write("Значит сегодня - " + day[day_number]);
