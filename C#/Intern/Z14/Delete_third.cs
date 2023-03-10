c20d6e66570dfe4b923690f0afff6cfa2a1b9a07
Glazyrin D <dglazyrin@gmail.com>
2023-03-10 16:02:07 +0300

    31-36 are done

--- C#/Intern/Z14/Delete_third.cs
+++ C#/Intern/Z14/Delete_third.cs
@@ -1,8 +1,8 @@
 ﻿int A = new Random().Next(1,1000);
 Console.Write("У нас есть число " + A + " и его третья цифра ");
-if (((A%1000-A%100)/100) > 0)
+if ((A/100)>0)
 {
-     Console.Write((A%1000-A%100)/100);
+     Console.Write((A%10));
 }
 else
 {
---
+++ C#/Senior/Z33/Program.cs
@@ -0,0 +1,23 @@
+﻿int[]FillMass(int[] X)        //Метод заполнения массива
+{
+     for (int i = 0; i < 12; i++)
+     {
+          X[i]=new Random().Next(0,10);
+     }
+     return X;
+}
+void ShowMass(int[] Y)         //Метод вывода массива
+{
+     Console.WriteLine("А массив у нас такой - ");
+     for (int i = 0; i < 12; i++)
+     {
+          Console.Write(Y[i] + " ");
+     }
+}
+///////////////
+// Каких положительных/отрицательных, если диапазон 0-9?
+///////////////
+
+int []MyMass = new int[12];
+FillMass (MyMass);
+ShowMass (MyMass);
