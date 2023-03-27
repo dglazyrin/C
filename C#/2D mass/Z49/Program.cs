dc6b4dc389c281f68b27d24e11368ff4481c1933
Glazyrin D <dglazyrin@gmail.com>
2023-03-27 12:49:32 +0300

    2D mass 49 is done

--- C#/2D mass/Z49/Program.cs
+++ C#/2D mass/Z49/Program.cs
@@ -1,25 +1,28 @@
 ﻿////// Массив 2D с вещественными числами
 
-int[,]FillMass(int[,] X)  //Метод заполнения массива
+double[,]FillMass(double[,] X)  //Метод заполнения массива
 {
+     Random x=new Random();
+     int n=0;
      for (int i = 0; i < X.GetLength(0); i++)
      {
           for (int j = 0; j < X.GetLength(1); j++)
           {
-          X[i,j]=new Random().Next(-99,100);
+          n = x.Next(1,7);
+          X[i,j]=Convert.ToDouble(x.Next(-100000000,100000000))/(Math.Pow(10,n));
           }
      }
      return X;
 }
 
-void ShowMass(int[,] X)  //Метод вывода массива
+void ShowMass(double[,] X)  //Метод вывода массива
 {
      Console.WriteLine("А массив у нас такой - ");
      for (int i = 0; i < X.GetLength(0); i++)
      {
           for (int j = 0; j < X.GetLength(1); j++)
           {
-               Console.Write("("+(i+1)+","+(j+1)+")"+X[i,j]+" ");
+               Console.Write("("+(i+1)+","+(j+1)+")"+X[i,j]+"  ");
           }
           Console.WriteLine();
      }
@@ -28,6 +31,6 @@ void ShowMass(int[,] X)  //Метод вывода массива
 int xSize=new Random().Next(2,11);
 int ySize=new Random().Next(2,11);
 Console.WriteLine("Массив " + xSize + " на " + ySize);
-int [,]MyMass = new int[xSize,ySize];
+double [,]MyMass = new double[xSize,ySize];
 FillMass (MyMass);
 ShowMass (MyMass);
